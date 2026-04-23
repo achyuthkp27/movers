8620b7cfc541043f020848f23cf63d58af3feb2d
Achyuth kp <kpachyuthz@gmail.com>
2026-04-23 20:16:43 +0530

    ci: add GitHub Actions workflow for automatic deployment to Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['three'],
+  output: 'export',
   images: {
-    domains: [],
+    unoptimized: true,
   },
+  transpilePackages: ['three'],
 };
 
 export default nextConfig;
